916fd594dc0475a6f51dcc4cf9b13d24d1822031
Vendy M8L <andyvenson99@gmail.com>
2025-08-06 11:50:01 -0400

    C'est bon pour la base. Maintenant on va ajouter le style poir les checkbox.

--- script.js
+++ script.js
@@ -1,89 +1,89 @@
 /** @format */
 
 // LES VARIABLES
-const unorderedList = document.getElementById("unordered-list");
-let placeholder = document.getElementById("placeholder");
-const enterTaskInput = document.getElementById("enter-task-input");
-const addTaskButton = document.getElementById("add-task-button");
-const deleteATaskButton = document.getElementById("delete-a-task-button");
-const deleteAllTaskButton = document.getElementById("delete-all-task-button");
+const unorderedList = document.getElementById("unordered-list")
+let placeholder = document.getElementById("placeholder")
+const enterTaskInput = document.getElementById("enter-task-input")
+const addTaskButton = document.getElementById("add-task-button")
+const deleteATaskButton = document.getElementById("delete-a-task-button")
+const deleteAllTaskButton = document.getElementById("delete-all-task-button")
 
 /*
  * LES FONCTIONS
  */
 // Fonction pour ajouter des tâches :
 function createNewTask(task, checked = false) {
-	deletePlaceholder();
+	deletePlaceholder()
 
-	let li = document.createElement("li");
-	let checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
-	li.appendChild(checkbox);
-	li.appendChild(document.createTextNode(" " + task));
-	unorderedList.appendChild(li);
+	let li = document.createElement("li")
+	let checkbox = document.createElement("input")
+	checkbox.type = "checkbox"
+	li.appendChild(checkbox)
+	li.appendChild(document.createTextNode(" " + task))
+	unorderedList.appendChild(li)
 
-	enterTaskInput.value = "";
-	enterTaskInput.focus();
+	enterTaskInput.value = ""
+	enterTaskInput.focus()
 
 	if (checked) {
-		checkbox.checked = true;
+		checkbox.checked = true
 	}
 }
 
 // Fonction pour ajouter le placeholder
 function newPlaceholder() {
-	placeholder = createPlaceholder();
-	unorderedList.appendChild(placeholder);
+	placeholder = createPlaceholder()
+	unorderedList.appendChild(placeholder)
 }
 
 // Fonction pour Désactiver les boutons s'il n'y a plus aucune tâches
 function disableButtons() {
-	let placeholder = document.getElementById("placeholder");
+	let placeholder = document.getElementById("placeholder")
 	if (
 		placeholder &&
 		unorderedList.contains(placeholder) &&
 		deleteATaskButton.dataset.mode === "saveMode"
 	) {
-		deleteATaskButton.disabled = true;
-		deleteAllTaskButton.disabled = true;
+		deleteATaskButton.disabled = true
+		deleteAllTaskButton.disabled = true
 	} else {
-		deleteATaskButton.disabled = false;
-		deleteAllTaskButton.disabled = false;
+		deleteATaskButton.disabled = false
+		deleteAllTaskButton.disabled = false
 	}
 }
 
 // Fonction pour créer le placeholder :
 function createPlaceholder() {
-	let li = document.createElement("li");
-	li.id = "placeholder";
-	let checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
-	checkbox.disabled = true;
-	li.appendChild(checkbox);
-	li.appendChild(document.createTextNode("..."));
-	placeholder = li;
+	let li = document.createElement("li")
+	li.id = "placeholder"
+	let checkbox = document.createElement("input")
+	checkbox.type = "checkbox"
+	checkbox.disabled = true
+	li.appendChild(checkbox)
+	li.appendChild(document.createTextNode("..."))
+	placeholder = li
 
-	return li;
+	return li
 }
 
 // Fonction pour supprimer le placeholder
 function deletePlaceholder() {
-	placeholder = document.getElementById("placeholder");
+	placeholder = document.getElementById("placeholder")
 	if (placeholder && unorderedList.contains(placeholder)) {
-		placeholder.remove();
-		placeholder = null;
+		placeholder.remove()
+		placeholder = null
 	}
 }
 
 // Fonction pour Sauvegarder les tâches dans le localStorage
 function storeTask(task) {
-	let taskListStored = JSON.parse(localStorage.getItem("taskList") || "[]");
-	let object1 = {};
-	object1.label = task;
-	object1.statut = "unchecked";
-	object1.category = "none";
-	taskListStored.push(object1);
-	sauvegarder(taskListStored);
+	let taskListStored = JSON.parse(localStorage.getItem("taskList") || "[]")
+	let object1 = {}
+	object1.label = task
+	object1.statut = "unchecked"
+	object1.category = "none"
+	taskListStored.push(object1)
+	sauvegarder(taskListStored)
 }
 
 // Fonction pour virer les accents et la casse
@@ -91,7 +91,7 @@ function normalizeText(text) {
 	return text
 		.normalize("NFD") // Décompose les accents
 		.replace(/[\u0300-\u036f]/g, "") // Supprime les diacritiques
-		.toUpperCase(); // Insensible à la casse
+		.toUpperCase() // Insensible à la casse
 }
 
 /*
@@ -100,92 +100,93 @@ function normalizeText(text) {
  * Vérifie si la tâche n'existe pas déjà
  */
 function taskVerification(task) {
-	let found = false;
+	let found = false
 	for (let li of unorderedList.querySelectorAll("li")) {
 		if (normalizeText(li.textContent.trim()) === normalizeText(task)) {
-			found = true;
-			break;
+			found = true
+			break
 		}
 	}
 	if (!task) {
-		alert("Entrez une tâche valide !");
-		return false;
+		alert("Entrez une tâche valide !")
+		return false
 	} else if (found) {
-		alert("Cette tâche existe déjà !");
-		enterTaskInput.value = "";
-		return false;
+		alert("Cette tâche existe déjà !")
+		enterTaskInput.value = ""
+		return false
 	} else {
-		return true;
+		return true
 	}
 }
 
 // Fonction pour recréer les elements pour supprimer l'ecouteur d'Event
 function recreateElements() {
 	document.querySelectorAll("li").forEach((li) => {
-		li.classList.remove("delete-mode");
-		let clone = li.cloneNode(true);
-		li.replaceWith(clone);
-	});
+		li.classList.remove("delete-mode")
+		let clone = li.cloneNode(true)
+		li.replaceWith(clone)
+	})
 }
 
 // Fonction pour supprimer une tache
 function deleteATask(task) {
-	let taskListDisplay = unorderedList.querySelectorAll("li");
-	deletePlaceholder();
-	let taskListStored = sync();
+	let taskListDisplay = unorderedList.querySelectorAll("li")
+	deletePlaceholder()
+	let taskListStored = sync()
 	let index = taskListStored.findIndex(
 		(t) => t.label === task.textContent.trim()
-	);
+	)
 
-	taskListStored.splice(index, 1);
-	return taskListStored;
+	taskListStored.splice(index, 1)
+	return taskListStored
 }
 
 // Fonction pour sauvegarder dans le localStorage
 function sauvegarder(taskListStored) {
-	localStorage.setItem("taskList", JSON.stringify(taskListStored));
+	localStorage.setItem("taskList", JSON.stringify(taskListStored))
 }
 
 // Fonction pour recuperer du le localStorage
 function sync() {
-	let taskListStored = JSON.parse(localStorage.getItem("taskList") || "[]");
-	return taskListStored;
+	let taskListStored = JSON.parse(localStorage.getItem("taskList") || "[]")
+	return taskListStored
 }
 
 // Fonction de suppression
 function deleteMode() {
-	let taskListDisplay = unorderedList.querySelectorAll("li");
+	let taskListDisplay = unorderedList.querySelectorAll("li")
 	taskListDisplay.forEach((task) => {
 		task.addEventListener("click", () => {
 			// Confirmation :
-			let hasConfirmed = false;
+			let hasConfirmed = false
 			hasConfirmed = confirm(
 				`Voulez-vous vraiment supprimer cette tache : ${task.textContent} ?`
-			);
+			)
 
 			if (hasConfirmed) {
-				let taskListStored = deleteATask(task);
-				task.remove();
-				let taskListDisplay = unorderedList.querySelectorAll("li");
-				if (taskListDisplay.length === 0) newPlaceholder();
-				sauvegarder(taskListStored);
+				let taskListStored = deleteATask(task)
+				task.remove()
+				let taskListDisplay = unorderedList.querySelectorAll("li")
+				if (taskListDisplay.length === 0) newPlaceholder()
+				sauvegarder(taskListStored)
 			}
-		});
-	});
+		})
+	})
 }
 
 // Fonction pour Désactiver les checkboxs
 function disableCheckboxes(disable) {
-	let checkboxes = unorderedList.querySelectorAll("input");
-	if (disable) {
-		checkboxes.forEach((checkbox) => {
-			checkbox.disabled = true;
-		});
-	} else {
-		checkboxes.forEach((checkbox) => {
-			checkbox.disabled = false;
-		});
-	}
+	let checkboxes = unorderedList.querySelectorAll("li input[type='checkbox']")
+	let placeholderElement = document.getElementById("placeholder")
+
+	checkboxes.forEach((checkbox) => {
+		// Toujours désactiver le placeholder
+		if (placeholderElement && placeholderElement.contains(checkbox)) {
+			checkbox.disabled = true
+		} else {
+			checkbox.disabled = disable
+		}
+	})
 }
 
 /*
@@ -196,38 +197,38 @@ function disableCheckboxes(disable) {
  */
 function toggleDeleteMode() {
 	if (deleteATaskButton.dataset.mode == "saveMode") {
-		deleteATaskButton.dataset.mode = "deleteMode";
-		deleteATaskButton.textContent = "Terminé";
-		addTaskButton.disabled = true;
-		enterTaskInput.disabled = true;
-		deleteAllTaskButton.disabled = true;
+		deleteATaskButton.dataset.mode = "deleteMode"
+		deleteATaskButton.textContent = "Terminé"
+		addTaskButton.disabled = true
+		enterTaskInput.disabled = true
+		deleteAllTaskButton.disabled = true
 
-		deleteMode();
-		disableCheckboxes(true);
+		deleteMode()
+		disableCheckboxes(true)
 	} else {
-		deleteATaskButton.dataset.mode = "saveMode";
-		deleteATaskButton.textContent = "Supprimer une tâche";
-		addTaskButton.disabled = false;
-		enterTaskInput.disabled = false;
-		deleteAllTaskButton.disabled = false;
+		deleteATaskButton.dataset.mode = "saveMode"
+		deleteATaskButton.textContent = "Supprimer une tâche"
+		addTaskButton.disabled = false
+		enterTaskInput.disabled = false
+		deleteAllTaskButton.disabled = false
 
-		recreateElements();
-		disableCheckboxes(true);
+		recreateElements()
+		disableCheckboxes(false)
 	}
 }
 
 // Fontion pour tout supprimer :
 function deleteAll() {
-	let hasConfirmed = false;
-	hasConfirmed = confirm("Voulez-vous vraiment TOUT supprimer ?");
+	let hasConfirmed = false
+	hasConfirmed = confirm("Voulez-vous vraiment TOUT supprimer ?")
 	if (hasConfirmed) {
-		localStorage.removeItem("taskList");
+		localStorage.removeItem("taskList")
 		unorderedList.querySelectorAll("li").forEach((li) => {
-			li.remove();
-		});
-		newPlaceholder();
+			li.remove()
+		})
+		newPlaceholder()
 
-		disableButtons();
+		disableButtons()
 	}
 }
 
@@ -239,42 +240,42 @@ function deleteAll() {
 
 enterTaskInput // Ajouter avec la touche entrer
 	.addEventListener("keypress", (e) => {
-		if (e.key === "Enter") addTaskButton.click();
-	});
+		if (e.key === "Enter") addTaskButton.click()
+	})
 
 // Recupérer le localStorage et placer les tâches
-let taskListStored = sync();
+let taskListStored = sync()
 if (taskListStored) {
 	taskListStored.forEach((task) => {
-		if(task.statut === "unchecked"){
-		createNewTask(task.label);
-		}else if(task.statut === "checked"){
+		if (task.statut === "unchecked") {
+			createNewTask(task.label)
+		} else if (task.statut === "checked") {
 			createNewTask(task.label, true)
 		}
-	});
+	})
 }
 
-disableButtons();
+disableButtons()
 
 // Ajouter des tâches au clic.
 addTaskButton.addEventListener("click", () => {
-	let taskListDisplay = unorderedList.querySelectorAll("li");
-	let newTask = enterTaskInput.value.trim();
-	let inputIsOkay = taskVerification(newTask);
-	if (!inputIsOkay) return;
-	createNewTask(newTask);
-	storeTask(newTask);
-	disableButtons();
-});
+	let taskListDisplay = unorderedList.querySelectorAll("li")
+	let newTask = enterTaskInput.value.trim()
+	let inputIsOkay = taskVerification(newTask)
+	if (!inputIsOkay) return
+	createNewTask(newTask)
+	storeTask(newTask)
+	disableButtons()
+})
 
 // Supprimer un élément
 deleteATaskButton.addEventListener("click", () => {
-	toggleDeleteMode();
-	disableButtons();
-});
+	toggleDeleteMode()
+	disableButtons()
+})
 
 // Supprimer tous les elements :
-deleteAllTaskButton.addEventListener("click", deleteAll);
+deleteAllTaskButton.addEventListener("click", deleteAll)
 /*
  * Afficher un log : C + A + L
  * Commenter ce log : A + S + C
@@ -284,16 +285,14 @@ deleteAllTaskButton.addEventListener("click", deleteAll);
 
 unorderedList.querySelectorAll("li").forEach((li) => {
 	li.addEventListener("change", () => {
-		let taskListStored = sync();
+		let taskListStored = sync()
 		let index = taskListStored.findIndex(
 			(t) => t.label === li.textContent.trim()
-		);
+		)
 
 		taskListStored[index].statut =
-			taskListStored[index].statut === "checked"
-				? "unchecked"
-				: "checked";
+			taskListStored[index].statut === "checked" ? "unchecked" : "checked"
 
-		sauvegarder(taskListStored);
-	});
-});
+		sauvegarder(taskListStored)
+	})
+})
